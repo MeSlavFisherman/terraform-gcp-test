aef57dba8897ed8643f51ee24343e3fff0e1cbdc
AzureAD\MislavJelusic <mislav.jelusic@syntio.net>
2021-08-20 14:25:06 +0200

    terraform.tfvars, variables.tf added

--- main.tf
+++ main.tf
@@ -1,20 +1,59 @@
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "3.5.0"
     }
   }
 }
 
 provider "google" {
-  credentials = file("syntio-aquarium-60a3ab4af9fb.json")
+  credentials = file(var.credentials_file)
+  #file("syntio-aquarium-60a3ab4af9fb.json")
 
-  project = "syntio-aquarium"
-  region  = "europe-west4"
-  zone    = "europe-west4-a"
+  project = var.project
+  region  = var.region
+  zone    = var.zone
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
+
+resource "google_compute_firewall" "default" {
+  name    = "test-firewall"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+
+  allow {
+    protocol = "udp"
+  }
+  allow {
+    protocol = "sctp"
+  }
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "8080", "1000-2000","22"]
+  }
 }
---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+project                  = "syntio-aquarium"
+credentials_file         = "syntio-aquarium-60a3ab4af9fb.json"
